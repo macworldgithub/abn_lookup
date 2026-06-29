31cee97f21a46dbe810dba151c0cb58ed97435f7
Mac World Technology <164502099+macworldgithub@users.noreply.github.com>
2026-06-29 10:57:06 +0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   server: {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 4090,
     env: process.env.NODE_ENV || 'development',
   },
   abn: {
